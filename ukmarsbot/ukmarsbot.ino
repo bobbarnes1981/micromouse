d00dbd0f069f5cf5cd3ffaf6343359d509d4127d
bobbarnes1981 <robertbarnes1981@googlemail.com>
2024-09-23 08:14:36 +0100

    Test encoder code from ukmarsbot

--- ukmarsbot/ukmarsbot.ino
+++ ukmarsbot/ukmarsbot.ino
@@ -1,3 +1,5 @@
+#include "digitalWriteFast.h"
+
 #define LED_L 11
 #define LED_R 6
 
@@ -16,11 +18,33 @@
 #define WALL_MIN_C 120
 #define WALL_MIN_R 120
 
+#define ENCODER_CLK_L 2
+#define ENCODER_CLK_R 3
+#define ENCODER_B_L 4
+#define ENCODER_B_R 5
+#define MOTOR_DIR_L 7
+#define MOTOR_DIR_R 8
+#define MOTOR_PWM_L 9
+#define MOTOR_PWM_R 10
+
 #define DELAY 100
 
 //minicom --device /dev/ttyUSB0 --baudrate 9600
 //minicom --device /dev/rfcomm0 --baudrate 9600
 //HC06 pin 1234
+
+unsigned long last_millis = 0;
+unsigned long elapsed_millis = 0;
+
+int wall_val_l = 0;
+int wall_val_c = 0;
+int wall_val_r = 0;
+
+int function_select;
+float battery_volts;
+
+volatile int encoderCountL;
+volatile int encoderCountR;
 
 void setup() {
   Serial.begin(9600);
@@ -35,19 +59,49 @@ void setup() {
   pinMode(EMITTERS, OUTPUT);
   pinMode(BATTERY_VOLTS, INPUT);
   pinMode(FUNC_SELECT, INPUT);
+
+  pinMode(ENCODER_CLK_L, INPUT);
+  pinMode(ENCODER_CLK_R, INPUT);
+
+  pinMode(ENCODER_B_L, INPUT);
+  pinMode(ENCODER_B_R, INPUT);
   
   digitalWrite(EMITTERS, LOW);
+
+  bitClear(EICRA, ISC01);
+  bitSet(EICRA, ISC00);
+  bitSet(EIMSK, INT0);
+  encoderCountL = 0;
+  
+  bitClear(EICRA, ISC11);
+  bitSet(EICRA, ISC10);
+  bitSet(EIMSK, INT1);
+  encoderCountR = 0;
 }
 
-unsigned long last_millis = 0;
-unsigned long elapsed_millis = 0;
+ISR(INT0_vect) {
+  static bool oldB = 0;
+  bool newB = bool(digitalReadFast(ENCODER_B_L));
+  bool newA = bool(digitalReadFast(ENCODER_CLK_L)) ^ newB;
+  if (newA == oldB) {
+    encoderCountL--;
+  } else {
+    encoderCountL++;
+  }
+  oldB = newB;
+}
 
-int wall_val_l = 0;
-int wall_val_c = 0;
-int wall_val_r = 0;
-
-int function_select;
-float battery_volts;
+ISR(INT1_vect) {
+  static bool oldB = 0;
+  bool newB = bool(digitalReadFast(ENCODER_B_R));
+  bool newA = bool(digitalReadFast(ENCODER_CLK_R)) ^ newB;
+  if (newA == oldB) {
+    encoderCountR++;
+  } else {
+    encoderCountR--;
+  }
+  oldB = newB;
+}
 
 void loop() {
   unsigned long m = millis();
@@ -74,8 +128,14 @@ void loop() {
     Serial.print(wall_val_c);
     Serial.print(" : ");
     Serial.print(wall_val_r);
+    Serial.print(" ");
+
+    Serial.print("EncL: ");
+    Serial.print(encoderCountL);
+    Serial.print(" ");
+    Serial.print("EncR: ");
+    Serial.print(encoderCountR);
     Serial.println("");
-  
   }
 
   digitalWrite(LED_L, wall_val_c > WALL_MIN_C || wall_val_l > WALL_MIN_L ? HIGH : LOW);
